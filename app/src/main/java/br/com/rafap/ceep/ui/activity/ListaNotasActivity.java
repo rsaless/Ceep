ec1c4ca0c61ca81a6d7dc0638683dff5c976445e
Rafael Sales Pavarina <rafael.pavarina@catijr.com.br>
2018-09-19 01:53:17 -0300

    avanço na implementação da RecyclerView

--- app/src/main/java/br/com/rafap/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/rafap/ceep/ui/activity/ListaNotasActivity.java
@@ -2,15 +2,17 @@ package br.com.rafap.ceep.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ListView;
 
 import java.util.List;
+import java.util.Random;
 
 import br.com.rafap.ceep.R;
 import br.com.rafap.ceep.dao.NotaDAO;
 import br.com.rafap.ceep.model.Nota;
-import br.com.rafap.ceep.ui.adapter.ListaNotasAdapter;
+import br.com.rafap.ceep.ui.recyclerview.adapter.ListaNotasAdapter;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -19,13 +21,34 @@ public class ListaNotasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
 
-        ListView listaNotas = findViewById(R.id.listView);
+
+        List<Nota> todasNotas = notasDeExemplo();
+
+        configuraRecyclerView(todasNotas);
+
+    }
+
+    private List<Nota> notasDeExemplo() {
         NotaDAO dao = new NotaDAO();
-        for (int i=1; i<= 10000; i++){
-            dao.insere(new Nota("titulo " + i,"descrição " + i));
+        Random gerador = new Random();
+        for (int i=1; i<= 100; i++){
+            String mensagem = geraMensagem(gerador.nextInt(40));
+            dao.insere(new Nota("titulo " + i,mensagem));
         }
 
-        List<Nota> todasNotas = dao.todos();
+        return dao.todos();
+    }
+
+    private String geraMensagem(int qtd){
+        String mensagem = "";
+        for (int i =0;i<qtd;i++) mensagem += "a";
+        return mensagem;
+    }
+
+    private void configuraRecyclerView(List<Nota> todasNotas) {
+        RecyclerView listaNotas = findViewById(R.id.lista_notas_recyclerview);
         listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        listaNotas.setLayoutManager(layoutManager);
     }
 }
---
+++ app/src/main/java/br/com/rafap/ceep/ui/recyclerview/adapter/ListaNotasAdapter.java
@@ -0,0 +1,57 @@
+package br.com.rafap.ceep.ui.recyclerview.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.com.rafap.ceep.R;
+import br.com.rafap.ceep.model.Nota;
+
+public class ListaNotasAdapter extends RecyclerView.Adapter<ListaNotasAdapter.NotaViewHolder> {
+
+    private final List<Nota> notas;
+    private final Context context;
+
+    public ListaNotasAdapter(Context context, List<Nota> notas){
+        this.context = context;
+        this.notas = notas;
+    }
+
+    @Override public ListaNotasAdapter.NotaViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View viewCriada = LayoutInflater.from(context).inflate(R.layout.item_nota, parent, false);
+        return new NotaViewHolder(viewCriada);
+    }
+
+    @Override
+    public void onBindViewHolder(ListaNotasAdapter.NotaViewHolder holder, int position) {
+        Nota nota = notas.get(position);
+        holder.vincula(nota);
+    }
+
+    @Override
+    public int getItemCount() {
+        return notas.size();
+    }
+
+    class NotaViewHolder extends RecyclerView.ViewHolder{
+
+        private final TextView titulo;
+        private final TextView descricao;
+
+        public NotaViewHolder(View itemView) {
+            super(itemView);
+            titulo = itemView.findViewById(R.id.item_nota_titulo);
+            descricao = itemView.findViewById(R.id.item_nota_descricao);
+        }
+
+        public void vincula(Nota nota){
+            titulo.setText(nota.getTitulo());
+            descricao.setText(nota.getDescricao());
+        }
+    }
+}
